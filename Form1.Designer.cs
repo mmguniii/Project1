a4aab2384adf1a052d5b36bb69c2e6613f6b17dd
mmguniii <gmemishova@std.beu.edu.az>
2025-09-25 23:14:57 +0400

    Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,126 +2,114 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
+
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            textBox1 = new TextBox();
-            password = new TextBox();
-            button1 = new Button();
-            label3 = new Label();
+            lblUser = new Label();
+            lblPass = new Label();
+            txtUser = new TextBox();
+            txtPass = new TextBox();
+            btnLogin = new Button();
+            lblInfo = new Label();
             SuspendLayout();
             // 
-            // label1
+            // lblUser
             // 
-            label1.AutoSize = true;
-            label1.BackColor = SystemColors.MenuHighlight;
-            label1.Font = new Font("Algerian", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.ForeColor = SystemColors.Control;
-            label1.Location = new Point(186, 201);
-            label1.Name = "label1";
-            label1.Size = new Size(83, 16);
-            label1.TabIndex = 0;
-            label1.Text = "Username";
+            lblUser.AutoSize = true;
+            lblUser.BackColor = Color.LightSkyBlue;
+            lblUser.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            lblUser.ForeColor = Color.Black;
+            lblUser.Location = new Point(150, 180);
+            lblUser.Name = "lblUser";
+            lblUser.Size = new Size(91, 23);
+            lblUser.TabIndex = 0;
+            lblUser.Text = "Istifadəçi:";
             // 
-            // label2
+            // lblPass
             // 
-            label2.AutoSize = true;
-            label2.BackColor = SystemColors.MenuHighlight;
-            label2.Font = new Font("Algerian", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.ForeColor = SystemColors.Control;
-            label2.Location = new Point(186, 256);
-            label2.Name = "label2";
-            label2.Size = new Size(83, 16);
-            label2.TabIndex = 1;
-            label2.Text = "Password";
-            label2.TextAlign = ContentAlignment.TopCenter;
+            lblPass.AutoSize = true;
+            lblPass.BackColor = Color.LightSkyBlue;
+            lblPass.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPass.ForeColor = Color.Black;
+            lblPass.Location = new Point(150, 235);
+            lblPass.Name = "lblPass";
+            lblPass.Size = new Size(69, 23);
+            lblPass.TabIndex = 1;
+            lblPass.Text = "Şifrə:";
             // 
-            // textBox1
+            // txtUser
             // 
-            textBox1.Location = new Point(298, 190);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 2;
+            txtUser.Location = new Point(270, 176);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(160, 27);
+            txtUser.TabIndex = 2;
             // 
-            // password
+            // txtPass
             // 
-            password.Location = new Point(298, 245);
-            password.Name = "password";
-            password.Size = new Size(125, 27);
-            password.TabIndex = 3;
-            password.UseSystemPasswordChar = true;
+            txtPass.Location = new Point(270, 231);
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(160, 27);
+            txtPass.TabIndex = 3;
+            txtPass.UseSystemPasswordChar = true;
             // 
-            // button1
+            // btnLogin
             // 
-            button1.BackColor = Color.Red;
-            button1.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.ForeColor = SystemColors.Control;
-            button1.Location = new Point(309, 306);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 4;
-            button1.Text = "Sign In";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
+            btnLogin.BackColor = Color.DarkGreen;
+            btnLogin.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Location = new Point(295, 290);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(110, 32);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Daxil ol";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
             // 
-            // label3
+            // lblInfo
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(186, 405);
-            label3.Name = "label3";
-            label3.Size = new Size(0, 20);
-            label3.TabIndex = 5;
+            lblInfo.AutoSize = true;
+            lblInfo.ForeColor = Color.DarkRed;
+            lblInfo.Location = new Point(180, 380);
+            lblInfo.Name = "lblInfo";
+            lblInfo.Size = new Size(0, 20);
+            lblInfo.TabIndex = 5;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(877, 619);
-            Controls.Add(label3);
-            Controls.Add(button1);
-            Controls.Add(password);
-            Controls.Add(textBox1);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            ClientSize = new Size(700, 500);
+            Controls.Add(lblInfo);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPass);
+            Controls.Add(txtUser);
+            Controls.Add(lblPass);
+            Controls.Add(lblUser);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Form1";
+            Text = "Login Paneli";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private TextBox textBox1;
-        private TextBox password;
-        private Button button1;
-        private Label label3;
+        private Label lblUser;
+        private Label lblPass;
+        private TextBox txtUser;
+        private TextBox txtPass;
+        private Button btnLogin;
+        private Label lblInfo;
     }
 }
